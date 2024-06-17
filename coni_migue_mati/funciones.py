508a6e5c2684335c60dfbf447b565ef292013122
miguelzzz123 <migueozz123@gmail.com>
2024-06-17 13:52:54 -0400

    el cambio

---
+++ coni_migue_mati/funciones.py
@@ -0,0 +1,8 @@
+def registrar_trabajador():
+    nombre = input("Ingrese el nombre y apellido del trabajador: ");
+    cargo = input("Ingrese el cargo del trabajador: ");
+    sueldo_bruto =0
+
+def listar_trabajadores():
+def imprimir_plantilla():
+    hola Matias
